1cf956f8d1770aac66288a4b5796cf14c3f1ce5a
Nikita Ilin <illinmikita@gmail.com>
2024-10-27 09:17:14 +0200

    ready

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-  // // Slick slider initialization
-  // $(".slider").slick({
-  //   infinite: true,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   speed: 300,
-  //   dots: false,
-  //   arrows: false,
-  //   autoplay: true, // Enables auto-slide
-  //   autoplaySpeed: 7000, // Auto-slide interval in milliseconds (5 seconds)
-  // });
+  // Slick slider initialization
+  $(".slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 300,
+    dots: false,
+    arrows: false,
+    autoplay: true, // Enables auto-slide
+    autoplaySpeed: 7000, // Auto-slide interval in milliseconds (5 seconds)
+  });
